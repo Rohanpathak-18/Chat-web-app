638c85a1d668c516bb33fbabf5bf1353c03c52cc
rohan kumar pathak <rohanpathak0171@gmail.com>
2026-08-13 17:00:36 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+  import.meta.env.VITE_API_URL || "https://chatsphere-amse.onrender.com/api";
 
 export const SOCKET_URL =
   import.meta.env.VITE_SOCKET_URL || "http://localhost:5001";
