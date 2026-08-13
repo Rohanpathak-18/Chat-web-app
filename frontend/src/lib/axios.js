47743594e6898556ea61a481954c1b49b8a4324e
rohan pathak <rohanpathak0171@gmail.com>
2026-08-13 22:36:13 +0530

    done...

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://chatsphere-amse.onrender.com/api";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://chatsphere-amse.onrender.com";
 
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "https://chatsphere-amse.onrender.com";
 
